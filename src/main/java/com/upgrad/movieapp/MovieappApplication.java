3120a3b62d192294a7a03fffcd1823579cd13f1a
Vishwa Mohan <vimohan@linkedin.com>
2021-01-30 01:54:33 +0530

    initial push

---
+++ src/main/java/com/upgrad/movieapp/MovieappApplication.java
@@ -0,0 +1,26 @@
+package com.upgrad.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class MovieappApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(MovieappApplication.class, args);
+
+
+	}
+
+	@RequestMapping(value="/")
+	public String helloStudents(){
+		return "Hello Students to Spring Boot !";
+	}
+
+}
