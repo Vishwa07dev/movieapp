4fa6c0c4d483e970d542c955544afa67815ab10f
Vishwa Mohan <vimohan@linkedin.com>
2021-02-01 00:42:18 +0530

    profiles

--- src/main/java/com/upgrad/movieapp/MovieappApplication.java
+++ src/main/java/com/upgrad/movieapp/MovieappApplication.java
@@ -1,7 +1,10 @@
 package com.upgrad.movieapp;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,14 @@ public class MovieappApplication {
 	public static void main(String[] args) {
 
 
-		SpringApplication.run(MovieappApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MovieappApplication.class, args);
+
+		System.out.println(context.getBeanDefinitionCount());
+
+		System.out.println(Arrays.stream(context.getBeanDefinitionNames()).
+				collect(Collectors.toList()).contains("myService"));
 
 
-	}
-
-	@RequestMapping(value="/")
-	public String helloStudents(){
-		return "Hello Students to Spring Boot !";
 	}
 
 }
---
+++ src/main/java/com/upgrad/movieapp/MyService.java
@@ -0,0 +1,10 @@
+package com.upgrad.movieapp;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+
+@Profile("qa")
+@Component
+public class MyService {
+}
