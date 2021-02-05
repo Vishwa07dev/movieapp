a04c66cb5e3ac7458652632e7aa7168022d333b9
Vishwa Mohan <vimohan@linkedin.com>
2021-02-05 11:50:23 +0530

    entity creation _ v 1.8

--- src/main/java/com/upgrad/movieapp/MovieappApplication.java
+++ src/main/java/com/upgrad/movieapp/MovieappApplication.java
@@ -1,12 +1,9 @@
 package com.upgrad.movieapp;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.context.ApplicationContext;
+		import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -15,15 +12,6 @@ public class MovieappApplication {
 
 	public static void main(String[] args) {
 
-
 		ApplicationContext context = SpringApplication.run(MovieappApplication.class, args);
-
-		System.out.println(context.getBeanDefinitionCount());
-
-		System.out.println(Arrays.stream(context.getBeanDefinitionNames()).
-				collect(Collectors.toList()).contains("myService"));
-
-
 	}
-
 }
