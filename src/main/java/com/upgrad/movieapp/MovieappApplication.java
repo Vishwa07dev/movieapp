b6d82a5ff89cfa576d8cf4bc5b5a32fba38d1793
Vishwa Mohan <vimohan@linkedin.com>
2021-02-26 03:50:04 +0530

    commit 2

--- src/main/java/com/upgrad/movieapp/MovieappApplication.java
+++ src/main/java/com/upgrad/movieapp/MovieappApplication.java
@@ -1,17 +1,10 @@
 package com.upgrad.movieapp;
 
-import com.upgrad.movieapp.dao.CustomerDAO;
-import com.upgrad.movieapp.dao.MovieDao;
-import com.upgrad.movieapp.entities.Customer;
-import com.upgrad.movieapp.entities.Movie;
-import com.upgrad.movieapp.services.MovieService;
-import java.time.LocalDateTime;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
